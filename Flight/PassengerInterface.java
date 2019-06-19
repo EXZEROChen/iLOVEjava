0a0e891b0bd2260a537c5e0429b30908ccaa77c3
EXZEROChen <cb294560475@163.com>
2019-06-19 23:00:53 +0800

    航班管理

---
+++ Flight/PassengerInterface.java
@@ -0,0 +1,12 @@
+package com.example.lib.JavaHomework.Passenger;
+
+public interface PassengerInterface {
+
+	public String getName();
+
+	public int getBookingNumber();
+
+	public int getRow();
+
+	public int getSeatPosition();
+}
